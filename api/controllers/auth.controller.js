84a9ea154ba2d2023412221b3bb44cea96505533
vasoyaprince14 <vasoyaprince14@gmail.com>
2024-02-19 18:56:37 +0530

    create a global catch

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,9 +1,11 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
-export const signup=async(req,res)=>{
+import { errorHandler } from "../utils/error.js";
+export const signup=async(req,res,next)=>{
     const {username, email, password}=req.body;
     if(!username|| !email||!password|| username===''||email===''||password===''){
-        return res.status(400).json({message :"all field required"});
+        // return res.status(400).json({message :"all field required"});
+        next(errorHandler(400,'All field required'));
     }
     const hashedPassword= await bcryptjs.hashSync(password,10);
     const newUser= new User({
@@ -18,8 +20,6 @@ export const signup=async(req,res)=>{
     })
     }
     catch(err){
-        res.status(500).json({
-            msg:"error agya"
-        })
+       next(err);
     }
 }
--- api/index.js
+++ api/index.js
@@ -11,6 +11,16 @@ mongoose.connect(process.env.MONGO).then(()=>{
 app.use(express.json());
 app.use("/api/user",userRoutes);
 app.use("/api/auth", authRoutes);
+
+app.use((err,req,res,next)=>{
+    const statusCode=err.statusCode||500;
+    const message=err.message|| 'Internal server error';
+    res.status(statusCode).json({
+        success:false,
+        statusCode,
+        message
+    })
+})
 app.listen(process.env.PORT,()=>{
     console.log(
         `server is running on port ${process.env.PORT} `
