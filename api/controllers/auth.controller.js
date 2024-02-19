61b24feabf1b09c2ca624eee3587266157741a6d
vasoyaprince14 <vasoyaprince14@gmail.com>
2024-02-19 18:42:03 +0530

    auth controller

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,25 @@
+import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+export const signup=async(req,res)=>{
+    const {username, email, password}=req.body;
+    if(!username|| !email||!password|| username===''||email===''||password===''){
+        return res.status(400).json({message :"all field required"});
+    }
+    const hashedPassword= await bcryptjs.hashSync(password,10);
+    const newUser= new User({
+        username,
+        email,
+        password:hashedPassword
+    })
+    try{
+        await newUser.save();
+    res.json({
+        msg:"sign succes"
+    })
+    }
+    catch(err){
+        res.status(500).json({
+            msg:"error agya"
+        })
+    }
+}
--- api/index.js
+++ api/index.js
@@ -2,13 +2,15 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from "dotenv";
 import userRoutes from './routes/user.route.js'
+import authRoutes from './routes/auth.route.js'
 const app=express();
 dotenv.config();
 mongoose.connect(process.env.MONGO).then(()=>{
     console.log("db connected");
 })
+app.use(express.json());
 app.use("/api/user",userRoutes);
-
+app.use("/api/auth", authRoutes);
 app.listen(process.env.PORT,()=>{
     console.log(
         `server is running on port ${process.env.PORT} `
